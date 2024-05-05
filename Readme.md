1d6f73bf044cab019355fb5737aa3708a2de7117
axel_jm <135370310+axeljm0@users.noreply.github.com>
2024-05-05 18:30:29 -0400

    Final update

---
+++ Readme.md
@@ -0,0 +1,22 @@
+Challenge encriptador de texto de Alura Latam.
+
+Se ejecuta dando click derecho al archivo index.html, y luego abrelo con tu navegador preferido.'
+
+Tenemos 2 funciones principales que son la de encriptar y desencriptar. EsTas dos funciones estan siendo mantenidas desde el archibo index.js.
+
+El archvo styles.css es el encargado de dar estilos a nuestro encriptador.
+
+               ---- Estuructur del codigo ----- 
+         
+     En la carpeta img encontraremos todas las imagenes necesarias para el proyecto.
+
+     En el index.html encontramos algunas etiquetas como lo son <H12> o <p> estas se encargan del titulos, subtitulos o texto respectivamente. Tambien hay alguinos div y un textArea que estructura de mejor manera lo se que quiere conseguir, un encriptador de texto atractivo y con colores primarios que le dan un toque sencillo. 
+
+     En el Styles.css tenemos los estilos principales y conseguimos un diseño adaptado a todos los dispositivos. 
+
+     Por ultimo en en nuestro index.js añadimos la logica necesaria para el funcionamiento de nuestro encriptador. Es lo que da vida a nuestra aplicaicon. Manipulamos el evento "onclick", y Capturamos nuestros elementos html y le aplicamos algunas condiciones, tambien con el metodo 
+     .replace, añadimos texto al introducir algunos parametros a nuestro textArea, todo con el objetivo de ayudar al usuario.
+
+
+
+     
---
+++ index.js
@@ -0,0 +1,46 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "Gracias por probarlo";
+    muñeco.src = "./img/encriptado.jpg";
+  } else {
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./img/desencriptado.jpg";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+}
+
